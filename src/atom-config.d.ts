0f2d667e68a3da0d2eba7976832f631f05845046
Nikolay Yakimov <root@livid.pp.ru>
2017-10-04 23:40:53 +0300

    Typed config

---
+++ src/atom-config.d.ts
@@ -0,0 +1,7 @@
+declare namespace AtomTypes {
+  interface ConfigInterface {
+    'autocomplete-haskell.defaultHintPanelVisibility': 'Visible' | 'Hidden'
+    'autocomplete-haskell.hideHintPanelIfEmpty': boolean
+    'autocomplete-haskell.showIdeHaskellTooltip': boolean
+  }
+}
